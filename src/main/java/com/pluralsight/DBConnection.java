c343f5cbfd39ac7a2efcd44c158224057db5c47c
JACJava <juliejava@icloud.com>
2019-05-26 15:02:32 -0500

    changes to work on mac:  changed com.mysql.cj.jdbc.Driver, updated create table routine to check for book table and fixed create string

--- src/main/java/com/pluralsight/DBConnection.java
+++ src/main/java/com/pluralsight/DBConnection.java
@@ -8,12 +8,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConnection {
-  
+
   private Connection jdbcConnection;
   private String jdbcURL = "jdbc:mysql://localhost:3306/book_store";
   private String jdbcUsername = "root";
   private String jdbcPassword = "root";
-  
+
   public DBConnection() {
     connect();
   }
@@ -34,44 +34,54 @@ public class DBConnection {
 //     System.exit(0);
 //   }
 // }
-//  
-  
- 
+//
+
+
   public void connect() {
 		try {
 			if (jdbcConnection == null || jdbcConnection.isClosed()) {
-				
-				Class.forName("com.mysql.jdbc.Driver");
-				
+
+				//Class.forName("com.mysql.jdbc.Driver");
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
 				jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-				
-				System.out.println("MySQL Connection Established");		
-			}		
+
+				System.out.println("MySQL Connection Established");
+
+				createTableIfNotExists();
+			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-  
- 
+
+
 
  private void createTableIfNotExists() {
    try {
        DatabaseMetaData meta = jdbcConnection.getMetaData();
-       ResultSet res = meta.getTables(null, null, null, new String[] {"TABLE"});
+
+       //ResultSet res = meta.getTables(null, null, null, new String[] {"TABLE"});
+       
+       ResultSet res = meta.getTables(null, "book_store", "book", new String[] {"TABLE"});
+
+//       while(res.next()) {
+//                System.out.println(res.getString(3));
+//          }
+//       
        Statement stmt = jdbcConnection.createStatement();
        if (!res.next()) {
        	// Create table
-
            String sql = "CREATE TABLE book " +
-                          "(id INTEGER PRIMARY KEY NOT NULL," +
-                          " title TEXT NOT NULL, " +
-                          " author TEXT NOT NULL, " +
-                          " price REAL)";
+                          "(id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+                          " title VARCHAR(255) NOT NULL, " +
+                          " author VARCHAR(1000) NOT NULL, " +
+                          " price DECIMAL(5,2))";
            stmt.executeUpdate(sql);
-
+      
            sql = "INSERT INTO book (title, author, price) VALUES (\"1984\", \"George Orwell\", 1.00)";
            stmt.executeUpdate(sql);
-
+      
            stmt.close();
        }
     } catch ( Exception e ) {
